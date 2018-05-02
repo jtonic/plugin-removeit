45af820704460b18538c18d1aeec9bb38e3e1cc9
jtonic <antonel.pazargic@gmail.com>
2018-05-02 08:18:06 +0300

    idea sandbox from repository org.gradle.java.home simple action added to WelcomeScreen to make idea freeze in the splash screen

--- src/main/java/ro/jtonic/handson/plugin/action/MyAction.java
+++ src/main/java/ro/jtonic/handson/plugin/action/MyAction.java
@@ -2,13 +2,11 @@ package ro.jtonic.handson.plugin.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.ui.Messages;
 
 public class MyAction extends AnAction {
 
   @Override
   public void actionPerformed(AnActionEvent e) {
-    System.out.println("e.isFromActionToolbar() = " + e.isFromActionToolbar());
-    Messages.showMessageDialog("Hello World!!!", "Message", Messages.getInformationIcon());
+    System.out.println("My action");
   }
 }
